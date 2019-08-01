fa3922ca646eee86f17625156f6a27049bcb79da
Host <morozov.alex.0001@gmail.com>
2019-08-01 13:39:45 +0300

    moved files

---
+++ app/controllers/controller.rb
@@ -0,0 +1,50 @@
+Dir['./app/models/*.rb'].each { |file| require file }
+Dir['./app/helpers/*.rb'].each { |file| require file }
+class Controller < Sinatra::Base
+  set views: proc { File.join(root, '../views/') }
+
+  register Sinatra::ActiveRecordExtension
+  register Sinatra::Flash
+  include UserHelper
+  include CommentHelper
+  include MessagesHelper
+  include RestaurantHelper
+
+  get '/' do
+    @restaurants = Restaurant.all
+    erb :home_page
+  end
+
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  get '/sign_in' do
+    erb :sign_in
+  end
+
+  post '/login' do
+    login
+    redirect '/'
+  end
+
+  get '/logout' do
+    logout
+    redirect env['HTTP_REFERER']
+  end
+
+  post '/rate' do
+    comment_to_the_cafe
+    redirect "/#{session[:cafe_name]}"
+  end
+
+  post '/registration' do
+    sign_up
+    redirect '/'
+  end
+
+  get '/:name' do
+    info_about_selected_cafe
+    erb :page_about_restaurant
+  end
+end
--- app/models/users.rb
+++ app/models/users.rb
@@ -2,13 +2,11 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   has_many :comments
   include BCrypt
-
   def password
     @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
+    self.password_hash = Password.create(new_password)
   end
 end
